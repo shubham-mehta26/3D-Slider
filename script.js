a7a32cedf6d0bc18e87b9e56b065fc97db21f8bc
Shubham Mehta <shubham.sm911@gmail.com>
2023-07-05 13:11:10 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+var TrendingSlider = new Swiper('.trending-slider', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    loop: true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+      rotate: 0,
+      stretch: 0,
+      depth: 100,
+      modifier: 2.5,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    }
+  });
